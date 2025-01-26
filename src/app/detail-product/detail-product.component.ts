4a2ccb391084db1703ed63878b06c44219421abd
Harounjlassi <jlassi.haroun@esprit.tn>
2025-01-27 00:07:38 +0100

    fix:add service to detail product

--- src/app/detail-product/detail-product.component.ts
+++ src/app/detail-product/detail-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-product',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-product.component.css']
 })
 export class DetailProductComponent implements OnInit {
+  constructor(private serviceroute:ActivatedRoute) {
+
+
+   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    console.log(this.serviceroute.snapshot.params['id']);
     
   }
 
