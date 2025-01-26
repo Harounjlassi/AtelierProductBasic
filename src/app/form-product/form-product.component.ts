49d55c8fee94ce1f772c32694bd99d3e259d59f4
Harounjlassi <jlassi.haroun@esprit.tn>
2025-01-26 11:25:35 +0100

    Fix:controllDe saisir

--- src/app/form-product/form-product.component.ts
+++ src/app/form-product/form-product.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
   styleUrls: ['./form-product.component.css']
 })
-export class FormProductComponent {
+export class FormProductComponent implements OnInit {
+product!:Product;
 
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.product= new Product();
+  
+}
 }
